5851ad8387087be29058aa8a2aab0cb8ec91495c
Choover <choover11@gmail.com>
2023-12-07 13:31:53 -0800

    added auth and prettier

--- src/app/api/graphql/route.ts
+++ src/app/api/graphql/route.ts
@@ -1,5 +1,5 @@
 // Next.js Custom Route Handler: https://nextjs.org/docs/app/building-your-application/routing/router-handlers
-import { createSchema, createYoga } from 'graphql-yoga'
+import { createSchema, createYoga } from 'graphql-yoga';
 
 const { handleRequest } = createYoga({
   schema: createSchema({
@@ -10,16 +10,21 @@ const { handleRequest } = createYoga({
     `,
     resolvers: {
       Query: {
-        greetings: () => 'This is the `greetings` field of the root `Query` type'
-      }
-    }
+        greetings: () =>
+          'This is the `greetings` field of the root `Query` type',
+      },
+    },
   }),
 
   // While using Next.js file convention for routing, we need to configure Yoga to use the correct endpoint
   graphqlEndpoint: '/api/graphql',
 
   // Yoga needs to know how to create a valid Next response
-  fetchAPI: { Response }
-})
+  fetchAPI: { Response },
+});
 
-export { handleRequest as GET, handleRequest as POST, handleRequest as OPTIONS }
+export {
+  handleRequest as GET,
+  handleRequest as POST,
+  handleRequest as OPTIONS,
+};
