cd5686e21805047210c966d0f9143211e17588e0
Choover <choover11@gmail.com>
2023-12-08 22:56:56 -0800

    Updated prisma schema, favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/icon.svg" sizes="any" />
+      </head>
       <body
         className={classNames(
           'min-h-screen bg-background font-sans antialiased',
